be8138b14a09ffdc129051a0091d46e295d67f6c
Nodnarbrones <sr.bvillasenor@gmail.com>
2022-11-14 18:41:17 -0800

    added to script.js file

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,1 +1,12 @@
 $('#currentDay').text(moment().format("MMM Do YY"))
+
+$(".saveBtn").on("click", function(){
+    var textvalue = $(this).siblings('.description').val(); 
+    var divKey = $(this).parent().attr('id');
+    window.localStorage.setItem(divKey, textvalue);
+})
+
+$('#9 .description').val(localStorage.getItem('9'));
+
+//hours and coloring
+
